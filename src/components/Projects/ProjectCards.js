8f3c45d6ae4bc51bd1b8c462b1f424ffefddd6d5
angelopol <114349306+angelopol@users.noreply.github.com>
2025-04-03 08:46:40 -0400

    projects modified

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -7,16 +7,18 @@ import { BsGithub } from "react-icons/bs";
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Img variant="top" src={props.imgPath} alt="card-img" className="rounded-circle rounded-2" />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
+        {props.ghLink && (
+          <Button variant="primary" href={props.ghLink} target="_blank">
+            <BsGithub /> &nbsp;
+            {props.isBlog ? "Blog" : "GitHub"}
+          </Button>
+        )}
         {"\n"}
         {"\n"}
 
@@ -30,7 +32,7 @@ function ProjectCards(props) {
             style={{ marginLeft: "10px" }}
           >
             <CgWebsite /> &nbsp;
-            {"Demo"}
+            {"Website"}
           </Button>
         )}
       </Card.Body>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import EZPASS from "../../Assets/Projects/EZPASSALL.png";
+import COLEABG from "../../Assets/Projects/COLEABGALL.png";
+import CARNETAPP from "../../Assets/Projects/CARNETAPPALL.png";
+import SOULBEAT from "../../Assets/Projects/SOULBEATALL.png";
+import BISTROT from "../../Assets/Projects/BISTROTALL.png";
+import FINANCY from "../../Assets/Projects/FINANCYALL.png";
+import INVENTORY from "../../Assets/Projects/INVENTORYALL.png";
 
 function Projects() {
   return (
@@ -23,68 +24,74 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={EZPASS}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="EZPASS"
+              description="Developed a web platform and microcomputer network for accounting and access control using Laravel, Bootstrap, JavaScript, Python, OpenCV, TtkBootstrap and MySQL. Integrated APIs and AWS for scalability. Built Python-based microcomputer software with offline database synchronization."
+              demoLink="https://ezpass.cloud/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={COLEABG}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Colegio de Abogados del Estado Carabobo Web Site"
+              description="Development of a web site for billing queries of the college members. Building with PHP, Bootstrap and Microsoft SQL Server."
+              ghLink="https://github.com/angelopol/ColeabgWebPage"
+              demoLink="https://coleabg.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={SOULBEAT}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="SOULBEAT"
+              description="Backend Developer. Designed MySQL database, developed migrations, models, and controllers in Laravel (MVC). Built real-time messaging with Pusher and sockets, working in an XP-based collaborative environment."
+              ghLink="https://github.com/angelopol/Soulbeat"
+              demoLink="https://soulbeat-main-aycgth.laravel.cloud/login"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={FINANCY}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="FINANCY"
+              description="Web app to manage your personal finances adapted to the Venezuelan market. Builded with Laravel, React, InertiaJs, Tailwind and MySQL."
+              ghLink="https://github.com/angelopol/financy"
+              demoLink="https://financy-main-sprvyq.laravel.cloud"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={BISTROT}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="Bistrot Chef Remy"
+              description="Backend Developer for Bistrot using NodeJs and Express. Developed user authentication, designed admin and employee templates, and structured routes and controllers. Coordinated backend-frontend integration following Scrum methodology."
+              ghLink="https://github.com/angelopol/bistrot"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={CARNETAPP}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="Carnets to Colegio de Abogados del Estado Carabobo"
+              description="Make Carnets to the College members using Python, OpenCV, Tkinter and MySQL."
+              ghLink="https://github.com/angelopol/ColeabgCarnetApp"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={INVENTORY}
+              isBlog={false}
+              title="INVENTORY"
+              description="App to manage a simple inventory using Python, Flet and SQLite."
+              ghLink="https://github.com/angelopol/inventory"
             />
           </Col>
         </Row>
