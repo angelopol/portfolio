1dc2c5d533b82d0b0d398a2c864b5b5997214013
angelopol <114349306+angelopol@users.noreply.github.com>
2025-04-03 09:54:53 -0400

    changed css styles

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -12,7 +12,7 @@ function Github() {
         username="angelopol"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        color="#076a1f"
         fontSize={16}
       />
     </Row>
