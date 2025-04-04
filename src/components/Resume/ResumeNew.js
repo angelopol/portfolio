55f7397c897a20c35979e93a8be8882458adf1bc
angelopol <114349306+angelopol@users.noreply.github.com>
2025-04-04 15:27:04 -0400

    cv updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Button } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/CV - Angelo Polgrossi - English.pdf";
+import pdf from "../../Assets/../Assets/CV - Angelo Polgrossi - English.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
- 
+
   useEffect(() => {
     setWidth(window.innerWidth);
   }, []);
@@ -29,12 +32,12 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-            <Document file={pdf} className="d-flex justify-content-center">
-              <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-           </Document>
-           <Document file={pdf} className="d-flex justify-content-center">
-              <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
-           </Document>
+          <Document file={pdf} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
+          <Document file={pdf} className="d-flex justify-content-center">
+            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
